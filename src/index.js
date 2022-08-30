789b683b775eae96fb455dc66422b95f6378dca0
Githmin Jayawardhana <githmin21@gmail.com>
2022-08-30 18:06:07 +0530

    inint webpack

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var input = "london";
+
+
+
+fetchLocation(input);
+
+async function fetchLocation(city){
+    try{
+        var locationFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7d2c7093f2814dbb6f630558dc52d5c5` ,{mode: 'cors'})
+        var locationJson = await locationFetch.json()
+        console.log(locationJson)
+        // console.log(locationJson.name , locationJson.main.temp)
+    } catch(err){
+        console.log(err)
+    }
+}
